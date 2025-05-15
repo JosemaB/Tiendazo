816b642d834ab83c3682e391ab3dc139c0cc6dc0
Jose Manuel <jmabenavides@gmail.com>
2025-05-15 14:56:02 +0200

    update navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
-import { Router, NavigationEnd , Event} from '@angular/router';
+import { Router, NavigationEnd, Event } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +11,20 @@ import { Router, NavigationEnd , Event} from '@angular/router';
 })
 export class AppComponent {
   title = 'tiendazo';
-
+  destroyRef = inject(DestroyRef);
   router = inject(Router);
+
   ngOnInit() {
-    this.router.events.subscribe((event: Event) => {
-      if (event instanceof NavigationEnd) {
-        setTimeout(() => {
-          if (typeof window !== 'undefined' && window.HSStaticMethods) {
-            window.HSStaticMethods.autoInit();
-          }
-        }, 500);
-      }
-    });
+    this.router.events
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((event: Event) => {
+        if (event instanceof NavigationEnd) {
+          setTimeout(() => {
+            if (typeof window !== 'undefined' && window.HSStaticMethods) {
+              window.HSStaticMethods.autoInit();
+            }
+          }, 500);
+        }
+      });
   }
 }
--- src/app/store/pages/products-search/products-search.component.ts
+++ src/app/store/pages/products-search/products-search.component.ts
@@ -1,4 +1,5 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { productCardComponent } from '@store/components/card/productCard.component';
 import { Product } from '@store/interfaces/product.interface';
@@ -11,12 +12,16 @@ import { Product } from '@store/interfaces/product.interface';
 export class ProductsSearchComponent {
   products = signal<Product[]>([]);
   route = inject(ActivatedRoute);
+  destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
-    this.route.params.subscribe(() => {
-      this.loadProducts();
-    });
+    this.route.params
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => {
+        this.loadProducts();
+      });
   }
+
   loadProducts() {
     window.scrollTo(0, 0);
     const productsJson = localStorage.getItem('products');
